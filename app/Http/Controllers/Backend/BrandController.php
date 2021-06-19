a3f97ba2765e96ee6c8e371c0070d15fa3a38d1c
Developer <mehmetakgul.developer@gmail.com>
2021-06-19 22:48:44 +0300

    For Brand Pages, 1-) Index , store, edit, update and delete functions were written to Brand Controller. 2-) The routes of these processes were created 3-) 'active' has been added to the class of the link clicked according to the url. 4-)sweettalet2 has been added and validation has been added for delete
    Marka Sayfaları için,
    1-) Brand Controller'e index , store,edit,update ve delete işlevleri yazıldı.
    2-)Bu işlemelerin rotaları oluşturuldu
    3-)url'ye göre tıklanan link'in class'ına 'active' eklendi.
    4-)sweettalet2 eklendi ve delete için doğrulama işlemi eklendi

---
+++ app/Http/Controllers/Backend/BrandController.php
@@ -0,0 +1,126 @@
+<?php
+
+namespace App\Http\Controllers\Backend;
+
+use App\Http\Controllers\Controller;
+use App\Models\Brand;
+use Illuminate\Http\Request;
+use Intervention\Image\Facades\Image;
+
+class BrandController extends Controller
+{
+    public function index()
+    {
+        $brands = Brand::latest()->get();
+        return view('backend.brand.index', compact('brands'));
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'brand_name_en' => 'required',
+            'brand_name_tr' => 'required',
+            'brand_image' => 'required',
+        ]);
+
+        $image = $request->file('brand_image');
+        $name_gen = hexdec(uniqid()) . '.' . $image->getClientOriginalExtension();
+        Image::make($image)->save('upload/brand/' . $name_gen);
+        $save_url = 'upload/brand/' . $name_gen;
+
+        $result = Brand::insert([
+            'brand_name_en' => $request->brand_name_en,
+            'brand_slug_en' => strtolower(str_replace(' ', '-', $request->brand_name_en)),
+            'brand_name_tr' => $request->brand_name_tr,
+            'brand_slug_tr' => strtolower(str_replace(' ', '-', $request->brand_name_tr)),
+            'brand_image' => $save_url,
+        ]);
+        if ($result) {
+            $notification = array(
+                'message' => 'Brand Added Successfully',
+                'alert-type' => 'success',
+            );
+        } else {
+            $notification = array(
+                'message' => 'An Error Occurred During Operation Please Contact Your System Administrator.',
+                'alert-type' => 'error',
+            );
+        }
+
+        return Redirect()->route('backend.brand.index')->with($notification);
+
+
+    }
+
+    public function edit($id)
+    {
+        $brand = Brand::find($id);
+        return view('backend.brand.edit', compact('brand'));
+    }
+
+    public function update(Request $request)
+    {
+        $result = "";
+        $request->validate([
+            'brand_name_en' => 'required',
+            'brand_name_tr' => 'required',
+        ]);
+
+        if ($request->file('brand_image')) {
+            $image = $request->file('brand_image');
+            $name_gen = hexdec(uniqid()) . '.' . $image->getClientOriginalExtension();
+            //$image->move(public_path('upload/brand/'), $name_gen);
+            Image::make($image)->save('upload/brand/' . $name_gen);
+
+            $save_url = 'upload/brand/' . $name_gen;
+            $result = Brand::findOrFail($request->id)->update([
+                'brand_name_en' => $request->brand_name_en,
+                'brand_slug_en' => strtolower(str_replace(' ', '-', $request->brand_name_en)),
+                'brand_name_tr' => $request->brand_name_tr,
+                'brand_slug_tr' => strtolower(str_replace(' ', '-', $request->brand_name_tr)),
+                'brand_image' => $save_url,
+            ]);
+            @unlink($request->old_image);
+        } else {
+            $result = Brand::findOrFail($request->id)->update([
+                'brand_name_en' => $request->brand_name_en,
+                'brand_slug_en' => strtolower(str_replace(' ', '-', $request->brand_name_en)),
+                'brand_name_tr' => $request->brand_name_tr,
+                'brand_slug_tr' => strtolower(str_replace(' ', '-', $request->brand_name_tr)),
+            ]);
+        }
+        if ($result) {
+            $notification = array(
+                'message' => 'Brand Successfully Updated',
+                'alert-type' => 'success',
+            );
+        } else {
+            $notification = array(
+                'message' => 'An Error Occurred During Operation Please Contact Your System Administrator.',
+                'alert-type' => 'error',
+            );
+        }
+
+        return Redirect()->route('backend.brand.index')->with($notification);
+    }
+
+    public function delete($id)
+    {
+        $brand = Brand::findOrFail($id);
+        $img = $brand->brand_image;
+        @unlink($img);
+        $result = Brand::findOrFail($id)->delete();
+        if ($result) {
+            $notification = array(
+                'message' => 'Brand Successfully Deleted',
+                'alert-type' => 'success',
+            );
+        } else {
+            $notification = array(
+                'message' => 'An Error Occurred During Operation Please Contact Your System Administrator.',
+                'alert-type' => 'error',
+            );
+        }
+        return Redirect()->back()->with($notification);
+    }
+}
